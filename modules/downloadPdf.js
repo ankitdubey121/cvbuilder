b2a0196f13fbe261bc4ffc8a25c18501a2785177
Ankit <ankitdubey8906@gmail.com>
2023-05-15 15:30:07 +0530

    Now creates directory if not present

--- modules/downloadPdf.js
+++ modules/downloadPdf.js
@@ -1,6 +1,26 @@
 const puppeteer = require('puppeteer')
 const uuid = require('uuid');
 const pdfName = `myPdf-${uuid.v4()}.pdf`;
+
+const fs = require('fs');
+
+const directoryPath = 'resumePdfs';
+
+// Check if the directory already exists
+if (!fs.existsSync(directoryPath)) {
+  // Directory does not exist, create it
+  fs.mkdir(directoryPath, (err) => {
+    if (err) {
+      console.error('Error creating directory:', err);
+    } else {
+      console.log('Directory created successfully');
+    }
+  });
+} else {
+  console.log('Directory already exists');
+}
+
+
 async function downloadPDF(url, fileName) {
     try{
         const browser = await puppeteer.launch();
